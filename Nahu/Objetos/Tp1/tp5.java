457682029fe0c9f1134b49d047ac345db2912d0b
Nahuel Hiriart <99101933+Nahuel0ff@users.noreply.github.com>
2024-09-21 17:13:11 -0400

    Update tp5.java

--- Nahu/Objetos/Tp1/tp5.java
+++ Nahu/Objetos/Tp1/tp5.java
@@ -1,16 +1,44 @@
 package ejercicio5Tp1;
 
-import PaqueteLectura.Lector;
+import PaqueteLectura.GeneradorAleatorio;
 
 
 public class ejercicio5 {
  
     public static void main(String[] args) {
+        GeneradorAleatorio.iniciar();
+         
+        int [][] matriz = new int [4][5];
+
+        for (int i=0;i<4;i++){
+         System.out.println("Cliente "+ (i+1)+" :");
+         for (int j=0;j<5;j++){
+           System.out.println("Ingrese la calificacion del 1 al 10 en la categoria: "+(j+1));
+           matriz[i][j]= GeneradorAleatorio.generarInt(11);
+         }
+        }
         
-        int [][] matriz = new int [5][4];
+        for (int i=0;i<4;i++){
+           double suma =0;
+           for (int j=0;j<5;j++){
+               suma +=matriz[i][j];
+           }
+          System.out.println("Los promedios son: "+suma/5);
+        }
+
+
+        
+    }
+    
+}
+
+
+
+
+
+     
+        /*
         double [] vectorProm = new double [4];
-        
-        
         for (int i=0;i<5;i++){
             System.out.println("Cliente " + (i + 1) + ":");
             for (int j=0;j<4;j++){
@@ -33,7 +61,7 @@ public class ejercicio5 {
         for (int i=0;i<4;i++){
           System.out.println("Los promedios son: "+vectorProm [i]);
         }
-        
-    }
-    
-}
+
+     */ 
+     //TODO ESTO ESTA MAL//
+
