c39823c242b24ab5aeda95f7eb1c29bb7742dda2
Lola <118538554+soylola@users.noreply.github.com>
2024-09-27 14:02:42 -0300

    Update P1E1.java

--- Lola/Objetos/Practica 1/P1E1.java
+++ Lola/Objetos/Practica 1/P1E1.java
@@ -1,1 +1,36 @@
+/*
 
+ Analice el programa Ej01Tabla2.java, que carga un vector que representa la tabla
+ del 2.
+ Genere enteros aleatorios hasta obtener el número 11. Para cada número muestre
+ el resultado de multiplicarlo por 2 (accediendo al vector).
+
+ */
+package Practica1;
+
+
+import PaqueteLectura.GeneradorAleatorio;
+
+public class P1E1 {
+
+    public static void main(String[] args) {
+        GeneradorAleatorio.iniciar(); //"mezclo el bolillero"
+       
+        int DF=11;  
+        int [] tabla = new int[DF]; // indices de 0 a 10;
+        int i, num;
+        
+        for (i=0;i<DF;i++){
+            num = GeneradorAleatorio.generarInt(12);
+            if (num != 11){
+                tabla[i]= num;
+                System.out.println("2x" +num+ "= "+ tabla[i]*2);
+            }
+            else{
+               System.out.println("Num es 11!");
+               i=11;
+            }
+        }
+    }
+    
+}
