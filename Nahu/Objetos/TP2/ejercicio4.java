0238c60e31b0d078ca09613dd58b124961af1093
Nahuel Hiriart <99101933+Nahuel0ff@users.noreply.github.com>
2024-09-29 19:47:31 -0300

    Update ejercicio4.java

--- Nahu/Objetos/TP2/ejercicio4.java
+++ Nahu/Objetos/TP2/ejercicio4.java
@@ -1,56 +1,58 @@
-package tema2;
-
+package practica2;
 import PaqueteLectura.Lector;
-
-public class Ejercicio4 {
-
-    public static void main(String[] args) {
-
-        Persona[][] matriz = new Persona[5][8];
-        int[] vectorCant = new int[5];
+public class ejercicio4 {
+    public static void main(String [] args){
+        Persona [][] matriz = new Persona[5][8];
+        Persona per;
+        int i;
+        int j;
+        int elementos = 0;
+        int turno;
+        int columna;
+        int cant;
         
-
-        for (int i = 0; i < 5; i++) {
-            vectorCant[i] = 0;
+        for(i = 0; i<5; i++){
+            for(j = 0; j<8; j++)
+                matriz[i][j] = null;
         }
-
-        int cant = 0;
-        String nom;
-        int edad, dni;
-
-        System.out.println("Ingrese nombre (ZZZ para finalizar): ");
-        nom = Lector.leerString();
-
-
-        while (cant < 40 && !nom.equals("ZZZ")) {
-            System.out.println("Ingrese edad: ");
-            edad = Lector.leerInt();
-            System.out.println("Ingrese DNI: ");
-            dni = Lector.leerInt();
-            System.out.println("Ingrese día (1 a 5): ");
-            int dia = Lector.leerInt() - 1;
-            if (dia >= 0 && dia < 5) {
-                if (vectorCant[dia] < 8) {
-                    matriz[dia][vectorCant[dia]] = new Persona(nom, dni, edad);
-                    vectorCant[dia]++;
-                    cant++;
-                } else {
-                    System.out.println("No quedan cupos disponibles en el día " + (dia + 1));
-                }
-            } else {
-                System.out.println("Día inválido. Debe estar entre 1 y 5.");
+        
+        System.out.print("Nombre: ");
+        String nombre = Lector.leerString();
+        while((elementos != 40) && (!nombre.equals("ZZZ"))){
+            System.out.print("Dni: ");
+            int dni = Lector.leerInt();
+            System.out.print("Edad: ");
+            int edad = Lector.leerInt();
+            System.out.print("Ingrese el dia en el que quiera sacar el turno: ");
+            turno = Lector.leerInt();
+            System.out.println("------------------------");
+            columna = 0;
+            while((columna < 8)&&(matriz[turno][columna] != null))
+                columna++;
+            if((columna < 8) &&(matriz[turno][columna] == null)){
+                per = new Persona(nombre, dni, edad);
+                matriz[turno][columna] = per;
+                elementos++;
+            }else
+                System.out.println("EL DIA EN EL QUE QUIERE SACAR EL TURNO YA ESTA LLENO.");
+            System.out.print("Nombre: ");
+            nombre = Lector.leerString();
+                 
+        }
+        
+        
+        
+        for(i= 0; i<5; i++){
+            cant = 0;
+            columna= 0;
+            while((columna < 8) &&(matriz[i][columna] != null)){
+                cant++;
+                System.out.print("("+i+","+columna+") "+matriz[i][columna].getNombre()+ " |");
+                columna++;
             }
-
-            System.out.println("Ingrese nombre (ZZZ para finalizar): ");
-            nom = Lector.leerString();
+            System.out.println("");
+            System.out.println("La cantidad de turnos el dia " + i+ " es de: "+ cant);
         }
-
-        System.out.println("Listado de inscriptos por día y turno:");
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Día " + (i + 1) + ": " + vectorCant[i] + " inscriptos.");
-            for (int j = 0; j < vectorCant[i]; j++) {
-                System.out.println("  Turno " + (j + 1) + ": " + matriz[i][j].getNombre());
-            }
-        }
+        
     }
 }
