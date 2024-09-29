74c1f4441a17bca2f347d01f2e5301238e99b9b2
Nahuel Hiriart <99101933+Nahuel0ff@users.noreply.github.com>
2024-09-29 18:17:45 -0300

    Create Ejercicio1.java

---
+++ Nahu/Objetos/TP3/Ejercicio1.java
@@ -0,0 +1,23 @@
+package tema3;
+
+import PaqueteLectura.Lector;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+    
+        
+        
+        double lado1 = Lector.leerDouble();
+        double lado2 = Lector.leerDouble();
+        double lado3 = Lector.leerDouble();
+        String fondo = Lector.leerString();
+        String linea = Lector.leerString();
+        
+        Triangulo triangulo = new Triangulo(lado1,lado2,lado3,fondo,linea);
+        
+        System.out.println(triangulo.calcularArea(lado2, lado3));
+        System.out.println(triangulo.calcularPerimetro(lado1, lado2, lado3));
+    }
+    
+}
