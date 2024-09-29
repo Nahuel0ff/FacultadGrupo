d12f813ecad2a4f2368b6d4127a16b5d6ff951b6
Nahuel Hiriart <99101933+Nahuel0ff@users.noreply.github.com>
2024-09-29 18:18:05 -0300

    Create Triangulo.java

---
+++ Nahu/Objetos/TP3/Triangulo.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tema3;
+
+/**
+ *
+ * @author nahue
+ */
+public class Triangulo {
+    private double lado1;
+    private double lado2;
+    private double lado3;
+    private String fondo;
+    private String linea;
+    
+    public Triangulo (double primerLado, double segundoLado, double tercerLado, String colorFondo, String colorLinea){
+        lado1 = primerLado;
+        lado2 = segundoLado;
+        lado3 = tercerLado;
+        fondo = colorFondo;
+        linea = colorLinea;
+    }
+    
+    public double getLado1(){
+        return lado1;
+    } 
+    
+    public double getLado2(){
+        return lado2;
+    } 
+    
+    public double getLado3(){
+        return lado3;
+    } 
+    
+    public String getFondo(){
+        return fondo;
+    }
+    
+    public String getLinea(){
+        return linea;
+    }
+    
+    public void setLado1(double primerLado){
+        lado1 = primerLado;
+    }
+
+     public void setLado2(double segundoLado){
+        lado2 = segundoLado;
+    }
+    
+    public void setLado3(double tercerLado){
+        lado3 = tercerLado;
+    }
+    
+    public void setFondo (String colorFondo){
+        fondo = colorFondo;
+    }
+    
+    public void setLinea (String colorLinea){
+        linea = colorLinea;
+    }
+    
+    public double calcularPerimetro(double primerLado, double segundoLado, double tercerLado){
+        double aux = lado1+lado2+lado3;
+        return (aux);
+    }
+    
+    public double calcularArea(double base, double altura){
+        double aux = (base * altura)/2;
+        return(aux);
+    }
+    
+    
+}
