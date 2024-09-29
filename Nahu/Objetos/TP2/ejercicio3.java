f85ccdd1a6a00562f14cd914a41b66ee64d2ba52
Nahuel Hiriart <99101933+Nahuel0ff@users.noreply.github.com>
2024-09-29 19:48:24 -0300

    Update ejercicio3.java

--- Nahu/Objetos/TP2/ejercicio3.java
+++ Nahu/Objetos/TP2/ejercicio3.java
@@ -2,37 +2,45 @@ package tema2;
 
 import PaqueteLectura.Lector;
 
-public class Ejercicio3 {
-
-    public static void main(String[] args) {
+public class ejercicio3 { //Las filas (dias) pueden no estar completas pero las columnas (turnos) siempre se completan
+    public static void main(String[]args){
+        Persona per;
+        Persona[][] matriz = new Persona[5][8];
+        int fila = 0;
+        int columna = 0;
+        int i;
+        int j;
+        String nom;
         
-        Persona [][] matriz = new Persona [5][8];
-        int cant = 0;
-        String nom = "";
-        int edad, dni;
-        
-        for (int i = 0; i < 5 && cant < 40 && !nom.equals("ZZZ"); i++) {
-            for (int j = 0; j < 8 && cant < 40 && !nom.equals("ZZZ"); j++) {
-                System.out.println("Ingrese nombre (ZZZ para finalizar): ");
-                nom = Lector.leerString();
-                if (!nom.equals("ZZZ")) { 
-                    System.out.println("Ingrese edad: ");
-                    edad = Lector.leerInt();
-                    System.out.println("Ingrese DNI: ");
-                    dni = Lector.leerInt();
-                    matriz[i][j] = new Persona(nom, dni, edad);
-                    cant++;
-                }
+        System.out.print("Nombre: ");
+        nom = Lector.leerString();
+        while((fila < 5)&&(!nom.equals("ZZZ"))){
+            System.out.print("Dni: ");
+            int dni = (Lector.leerInt());
+            System.out.print("Edad: ");
+            int edad = (Lector.leerInt());
+            per = new Persona(nom,dni,edad);
+            matriz [fila][columna] = per;
+            columna++;
+            if(columna == 8){
+                columna=0;
+                fila++;
             }
-        }
+            System.out.println("-------------------");
+            System.out.print("Nombre: ");
+            nom = Lector.leerString();
+            
+        } 
         
-        System.out.println("Listado de inscriptos por día y turno:");
-        for (int i = 0; i < 5; i++) {  
-            for (int j = 0; j < 8; j++) {  
-                if (matriz[i][j] != null) {
-                    System.out.println("Día " + (i + 1) + ", Turno " + (j + 1) + ": " + matriz[i][j].getNombre());
-                }
+        for(i = 0; i<fila; i++){
+            System.out.println("---Dia---"+i);
+            for(j = 0; j<8; j++){
+              System.out.println("-Turno: "+j);
+              System.out.println("El nombre de la persona a entrevistar ese día y turno es: " + matriz[i][j].getNombre());
+              System.out.println();
             }
+           
         }
+      }
     }
-}
+        
