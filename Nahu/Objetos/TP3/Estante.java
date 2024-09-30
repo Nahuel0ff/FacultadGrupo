b2105031b522352f00975a06c7ef8044b89b6f9f
Nahuel Hiriart <99101933+Nahuel0ff@users.noreply.github.com>
2024-09-30 10:52:42 -0300

    Create Estante.java

---
+++ Nahu/Objetos/TP3/Estante.java
@@ -0,0 +1,41 @@
+
+package tema3;
+
+
+public class Estante {
+    private Libro[] vectorLibros;
+    private int dimF;
+    private int dimL;
+    
+    public Estante (int capacidad){
+        this.dimF=capacidad;
+        this.dimL=0;
+        this.vectorLibros=new Libro[dimF];
+    }
+    
+    public int getCantLibros (){
+        return dimL;
+    }
+    
+    public boolean estaLleno(){
+        return dimL == dimF;
+    }
+    
+    public void agregarLibro(Libro libroAdd) {
+        if (!estaLleno()) {
+            vectorLibros[dimL] = libroAdd;
+            dimL++;
+        } else {
+            System.out.println("El estante está lleno. No se puede agregar más libros.");
+        }
+    }
+    
+     public Libro buscarLibro(String titulo) {
+        for (int i = 0; i < dimL; i++) {
+            if (vectorLibros[i].getTitulo().equals(titulo)) {
+                return vectorLibros[i];
+            }
+        }
+        return null;
+    }
+}
