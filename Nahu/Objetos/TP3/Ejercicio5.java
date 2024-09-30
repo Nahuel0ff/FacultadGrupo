66b7eb1c823a12455df3c8a5103bf163746dfb0b
Nahuel Hiriart <99101933+Nahuel0ff@users.noreply.github.com>
2024-09-30 20:37:07 -0300

    Create Ejercicio5.java

---
+++ Nahu/Objetos/TP3/Ejercicio5.java
@@ -0,0 +1,19 @@
+package tema3;
+
+
+public class Ejercicio5 {
+
+    
+    public static void main(String[] args) {
+        
+        
+        Circulo cir = new Circulo(4.0,"Rojo","Azul");
+        
+        System.out.println(cir.calcularArea());
+        System.out.println(cir.calcularPerimetro());
+        
+        
+        
+    }
+    
+}
