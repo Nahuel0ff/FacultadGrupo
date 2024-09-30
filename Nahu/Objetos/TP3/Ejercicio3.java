44987bd208268843c2ea9232c2c6d74bbed8424c
Nahuel Hiriart <99101933+Nahuel0ff@users.noreply.github.com>
2024-09-30 10:52:57 -0300

    Create Ejercicio3.java

---
+++ Nahu/Objetos/TP3/Ejercicio3.java
@@ -0,0 +1,38 @@
+package tema3;
+
+
+public class Ejercicio3 {
+
+   
+    public static void main(String[] args) {
+    
+      Estante estant = new Estante(20);
+      Libro uno = new Libro();
+      uno.setTitulo("Java 4 Dummies");
+      Libro dos = new Libro();
+      dos.setTitulo("Mujercitas");
+      Libro tres = new Libro();
+      tres.setTitulo("Pascal 4 Dummies");
+      Libro cuatro = new Libro();
+      cuatro.setTitulo("Creo que R-Info no tiene libro, lee la documentacion");
+      estant.agregarLibro(uno);
+      estant.agregarLibro(dos);
+      estant.agregarLibro(tres);
+      estant.agregarLibro(cuatro);
+      Libro libroBuscado = estant.buscarLibro("Mujercitas");
+      if (libroBuscado != null){
+          System.out.println("Se encontro el libro Mujercitas");
+       } else {
+            System.out.println("El libro 'Mujercitas' no se encontró en el estante.");
+        }
+      
+      System.out.println(""+estant.getCantLibros());
+   
+        
+        
+        
+        
+       
+    }
+    
+}
