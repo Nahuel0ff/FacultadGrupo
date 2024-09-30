a2d7d61436daef6cd189fef289ef3189a1ffa17d
Nahuel Hiriart <99101933+Nahuel0ff@users.noreply.github.com>
2024-09-30 12:39:21 -0300

    Create Cliente.java

---
+++ Nahu/Objetos/TP3/Ejercicio4/Cliente.java
@@ -0,0 +1,42 @@
+
+package tema3;
+
+
+public class Cliente {
+    private String nombre;
+    private int DNI;
+    private int edad;
+    
+    
+    public Cliente (String unNombre, int unDNI, int unaEdad){
+        nombre = unNombre;
+        DNI = unDNI;
+        edad = unaEdad;        
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public int getDNI(){
+        return DNI;
+    }
+    
+    public int getEdad(){
+        return edad;
+    }
+    
+    public void setNombre(String unNombre){
+        nombre = unNombre;
+    }
+    
+    public void setDNI(int unDNI){
+        DNI = unDNI;
+    }
+    
+    public void setEdad(int unaEdad){
+        edad = unaEdad;
+    }
+    
+    
+}
