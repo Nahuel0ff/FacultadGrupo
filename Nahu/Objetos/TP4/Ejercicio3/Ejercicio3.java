419da06d47e108de6ec2a6c69993a747d7ea54a3
Nahuel Hiriart <99101933+Nahuel0ff@users.noreply.github.com>
2024-10-04 15:10:51 -0300

    Update Ejercicio3.java

--- Nahu/Objetos/TP4/Ejercicio3/Ejercicio3.java
+++ Nahu/Objetos/TP4/Ejercicio3/Ejercicio3.java
@@ -1,1 +1,17 @@
 
+package tema4;
+
+
+public class Ejercicio3 {
+
+   
+    public static void main(String[] args) {
+        
+        Persona persona1 = new Persona (44333111,"Jose",43);
+        Trabajador jardinero = new Trabajador (34555222,"Miguel",65,"Jardinero");
+        
+        System.out.println(persona1.toString());
+        System.out.println(jardinero.toString());
+    }
+    
+}
