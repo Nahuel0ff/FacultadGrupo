5aee94e611d27bf78803b2e1d918ee631ee853a9
Nahuel Hiriart <99101933+Nahuel0ff@users.noreply.github.com>
2024-10-04 12:31:00 -0300

    Update Ejercicio3.java

--- Nahu/Objetos/TP3/Ejercicio3.java
+++ Nahu/Objetos/TP3/Ejercicio3.java
@@ -1,39 +1,39 @@
 package tema3;
 
-
 public class Ejercicio3 {
 
-   // ESTA MAL HECHO, EL CONSTRUCTOR DEBERIA RECIBIR EL "PARAMETRO" EN LUGAR DE SETEARLO
-   // NO SE BUSCA EL AUTOR COMO PIDE LA CONSIGNA, FALTAN EJERCICIOS
     public static void main(String[] args) {
-    
-      Estante estant = new Estante(20);
-      Libro uno = new Libro();
-      uno.setTitulo("Java 4 Dummies");
-      Libro dos = new Libro();
-      dos.setTitulo("Mujercitas");
-      Libro tres = new Libro();
-      tres.setTitulo("Pascal 4 Dummies");
-      Libro cuatro = new Libro();
-      cuatro.setTitulo("Creo que R-Info no tiene libro, lee la documentacion");
-      estant.agregarLibro(uno);
-      estant.agregarLibro(dos);
-      estant.agregarLibro(tres);
-      estant.agregarLibro(cuatro);
-      Libro libroBuscado = estant.buscarLibro("Mujercitas");
-      if (libroBuscado != null){
-          System.out.println("Se encontro el libro Mujercitas");
-       } else {
+
+        Estante estante = new Estante(20);
+        
+        Libro uno = new Libro();
+        uno.setTitulo("Java 4 Dummies");
+        uno.setPrimerAutor(new Autor("Autor1", "Biografía del autor1", "Origen1"));
+
+        Libro dos = new Libro();
+        dos.setTitulo("Mujercitas");
+        dos.setPrimerAutor(new Autor("Louisa May Alcott", "Escritora estadounidense", "EE.UU"));
+
+        Libro tres = new Libro();
+        tres.setTitulo("Pascal 4 Dummies");
+        tres.setPrimerAutor(new Autor("Autor2", "Biografía del autor2", "Origen2"));
+
+        Libro cuatro = new Libro();
+        cuatro.setTitulo("Creo que R-Info no tiene libro, lee la documentacion");
+        cuatro.setPrimerAutor(new Autor("Autor3", "Biografía del autor3", "Origen3"));
+
+        estante.agregarLibro(uno);
+        estante.agregarLibro(dos);
+        estante.agregarLibro(tres);
+        estante.agregarLibro(cuatro);
+
+        Libro libroBuscado = estante.buscarLibro("Mujercitas");
+        if (libroBuscado != null) {
+            System.out.println("Se encontró el libro 'Mujercitas'. Autor: " + libroBuscado.getPrimerAutor().getNombre());
+        } else {
             System.out.println("El libro 'Mujercitas' no se encontró en el estante.");
         }
-      
-      System.out.println(""+estant.getCantLibros());
-   
-        
-        
-        
-        
-       
+
+        System.out.println("Cantidad de libros en el estante: " + estante.getCantLibros());
     }
-    
 }
