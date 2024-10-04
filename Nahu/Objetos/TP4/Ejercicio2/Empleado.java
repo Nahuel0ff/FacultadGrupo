26e78a85b642e614855a827e945dddd70b0bd377
Nahuel Hiriart <99101933+Nahuel0ff@users.noreply.github.com>
2024-10-04 11:04:21 -0300

    Create Empleado.java

---
+++ Nahu/Objetos/TP4/Ejercicio2/Empleado.java
@@ -0,0 +1,53 @@
+package tema4;
+
+public abstract class Empleado {
+    private String nombre;
+    private double sueldo;
+    private int antiguedad;
+    
+    public Empleado (String unNombre, double unSueldo, int unAño){
+        setNombre(unNombre);
+        setSueldo(unSueldo);
+        setAntiguedad(unAño);
+    }
+    
+    private void setNombre(String unNombre){
+        nombre = unNombre;
+    } 
+    
+    private void setSueldo(double unSueldo){
+        sueldo = unSueldo;
+    }
+    
+    private void setAntiguedad (int unAño){
+        antiguedad = unAño;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public double getSueldo(){
+        return sueldo;
+    }
+    
+    public int getAntiguedad(){
+        return antiguedad;
+    }
+    
+    public abstract double calcularEfectividad();
+    
+    public double calcularSueldoACobrar() {
+        double aux = getSueldo() + ((getSueldo())*0.10)*getAntiguedad();
+        return aux;
+    }
+    
+    public String toString(){
+        String aux = "El empleado: "+getNombre()+
+                     " debe cobrar: "+this.calcularSueldoACobrar()+
+                     " y tiene una efectividad de "+this.calcularEfectividad();
+        return aux;
+    }
+    
+    
+}
