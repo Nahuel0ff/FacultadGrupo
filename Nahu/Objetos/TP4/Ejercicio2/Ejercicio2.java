3784c84c6dffd6988df4676027c9d38f0c2d6196
Nahuel Hiriart <99101933+Nahuel0ff@users.noreply.github.com>
2024-10-04 11:03:35 -0300

    Update Ejercicio2.java

--- Nahu/Objetos/TP4/Ejercicio2/Ejercicio2.java
+++ Nahu/Objetos/TP4/Ejercicio2/Ejercicio2.java
@@ -1,1 +1,20 @@
 
+package tema4;
+
+
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+    
+        Jugador delantero = new Jugador("Messi",1000000.00,20,500,350);
+        
+        Entrenador director = new Entrenador("Maurinho",50000.00,35,10); 
+        
+         System.out.println(director.toString());
+        
+         System.out.println(delantero.toString());
+        
+        
+    }
+    
+}
