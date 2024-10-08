3ae2f08f309d1d6b4afe8ae91a995530377278b7
Nahuel Hiriart <99101933+Nahuel0ff@users.noreply.github.com>
2024-10-08 15:10:26 -0300

    Create Ejercicio1.java

---
+++ Nahu/Objetos/TP5/Ejercicio1/Ejercicio1.java
@@ -0,0 +1,15 @@
+
+package ejercicio1practica5;
+
+public class Ejercicio1Practica5 {
+
+    public static void main(String[] args) {
+     
+        
+        
+        
+        
+        
+    }
+    
+}
